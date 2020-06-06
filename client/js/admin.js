57959ce07b349019b7422879f7d0c0051cdc658d
AntonKozmod <antoxa.kozmod@gmail.com>
2020-06-06 13:19:02 +0300

    FIX admin.js (в поапрошлый раз закоммитился не тот admin.js

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,74 +1,65 @@
-var organizeByTags = function (toDoObjects) { 
-	// создание пустого массива для тегов
-	var tags = [];
-	// перебираем все задачи toDos 
-	toDoObjects.forEach(function (toDo) {
-		// перебираем все теги для каждой задачи 
-		toDo.tags.forEach(function (tag) {
-			// убеждаемся, что этого тега еще нет в массиве
-			if (tags.indexOf(tag) === -1) { 
-				tags.push(tag);
-			}
-		});
-	}); 
-	var tagObjects = tags.map(function (tag) {
-		// здесь мы находим все задачи,
-		// содержащие этот тег
-		var toDosWithTag = []; 
-		toDoObjects.forEach(function (toDo) {
-			// проверка, что результат
-			// indexOf is *не* равен -1
-			if (toDo.tags.indexOf(tag) !== -1) { 
-				toDosWithTag.push(toDo.description);
-			}
-		});
-		// мы связываем каждый тег с объектом, который содержит название тега и массив
-		return { "name": tag, "toDos": toDosWithTag };
-	});
-	return tagObjects;
-};
-
-var liaWithEditOrDeleteOnClick = function (todo, callback) {
-	var $todoListItem = $("<li>").text(todo.description),
-		$todoEditLink = $("<a>").attr("href", "todos/" + todo._id),
-		$todoRemoveLink = $("<a>").attr("href", "todos/" + todo._id);
-
-	$todoEditLink.addClass("linkEdit");
-	$todoRemoveLink.addClass("linkRemove");
-
-	$todoRemoveLink.text("Удалить");
-	$todoRemoveLink.on("click", function () {
-		$.ajax({
-			url: "/todos/" + todo._id,
-			type: "DELETE"
-		}).done(function (responde) {
-			callback();
-		}).fail(function (err) {
-			console.log("error on delete 'todo'!");
-		});
-		return false;
-	});
-	$todoListItem.append($todoRemoveLink);
-
-	$todoEditLink.text("Редактировать");
-	$todoEditLink.on("click", function() {
-		var newDescription = prompt("Введите новое наименование для задачи", todo.description);
-		if (newDescription !== null && newDescription.trim() !== "") {
+var liaWithDeleteOnClick = function (user, callback) {
+	var $usersListItem = $("<li>").text(user.username);
+	
+	var $usersRemoveLink = $("<a>").attr("href", "#");
+	$usersRemoveLink.addClass("linkRemove");
+	$usersRemoveLink.text("Удалить");
+	$usersRemoveLink.on("click", function () {
+		if (confirm("Вы действительно хотите удалить пользователя " + user.username + "?")) {
 			$.ajax({
-				"url": "/todos/" + todo._id,
-				"type": "PUT",
-				"data": { "description": newDescription },
+				url: "/users/" + user._id,
+				type: "DELETE"
 			}).done(function (responde) {
 				callback();
 			}).fail(function (err) {
-				console.log("Произошла ошибка: " + err);
+				console.log("error on delete 'user'!");
 			});
+			return false;
 		}
-		return false;
 	});
-	$todoListItem.append($todoEditLink);
+	$usersListItem.append($usersRemoveLink);
 
-	return $todoListItem;
+	if (!user.doctor) {
+		var $toTheDoctorLink = $("<a>").attr("href", "#");
+		$toTheDoctorLink.addClass("link");
+		$toTheDoctorLink.text("Сделать врачом");
+		$toTheDoctorLink.on("click", function() {
+			if (confirm("Вы действительно хотите пользователя " + user.username + " назначить врачом?")) {
+				$.ajax({
+					url: "/users/" + user._id,
+					type: "PUT",
+					data: { "doctor": true }
+				}).done(function (responde) {
+					callback();
+				}).fail(function (err) {
+					console.log("error! " + err);
+				});
+				return false;
+			}
+		});
+		$usersListItem.append($toTheDoctorLink);
+	} else {
+		var $toTheDoctorLink = $("<a>").attr("href", "#");
+		$toTheDoctorLink.addClass("link");
+		$toTheDoctorLink.text("Убрать из врачей");
+		$toTheDoctorLink.on("click", function() {
+			if (confirm("Вы действительно хотите пользователя " + user.username + " убрать из врачей?")) {
+				$.ajax({
+					url: "/users/" + user._id,
+					type: "PUT",
+					data: { "doctor": false }
+				}).done(function (responde) {
+					callback();
+				}).fail(function (err) {
+					console.log("error! " + err);
+				});
+				return false;
+			}
+		});
+		$usersListItem.append($toTheDoctorLink);
+	}
+
+	return $usersListItem;
 }
 
 var main = function () {
@@ -77,19 +68,21 @@ var main = function () {
 	var tabs = [];
 	// добавляем вкладку Новые
 	tabs.push({
-		"name": "Новые",
+		"name": "Новые пациенты",
 		// создаем функцию content
 		// так, что она принимает обратный вызов
 		"content": function(callback) {
-			$.getJSON("todos.json", function (toDoObjects) {
+			$.getJSON("/users.json", function (usersObjects) {
 				var $content,
 					i;
 				$content = $("<ul>");
-				for (i = toDoObjects.length-1; i>=0; i--) {
-					var $todoListItem = liaWithEditOrDeleteOnClick(toDoObjects[i], function() {
-						$(".tabs a:first-child span").trigger("click");
-					});
-					$content.append($todoListItem);
+				for (i = usersObjects.length-1; i>=0; i--) {
+					if (!usersObjects[i].admin && !usersObjects[i].doctor) {
+						var $usersListItem = liaWithDeleteOnClick(usersObjects[i], function() {
+							$(".tabs a:first-child span").trigger("click");
+						});
+						$content.append($usersListItem);
+					}
 				}
 				callback(null, $content);
 			}).fail(function (jqXHR, textStatus, error) {
@@ -100,17 +93,19 @@ var main = function () {
 
 	// добавляем вкладку Старые
 	tabs.push({
-		"name": "Старые",
+		"name": "Старые пациенты",
 		"content": function(callback) {
-			$.getJSON("todos.json", function (toDoObjects) {
+			$.getJSON("/users.json", function (usersObjects) {
 				var $content,
 					i;
 				$content = $("<ul>");
-				for (i = 0; i < toDoObjects.length; i++) {
-					var $todoListItem = liaWithEditOrDeleteOnClick(toDoObjects[i], function() {
-						$(".tabs a:nth-child(2) span").trigger("click");
-					});
-					$content.append($todoListItem);
+				for (i = 0; i < usersObjects.length; i++) {
+					if (!usersObjects[i].admin && !usersObjects[i].doctor) {
+						var $usersListItem = liaWithDeleteOnClick(usersObjects[i], function() {
+							$(".tabs a:nth-child(2) span").trigger("click");
+						});
+						$content.append($usersListItem);
+					}
 				}
 				callback(null, $content);
 			}).fail(function(jqXHR, textStatus, error) {
@@ -119,63 +114,25 @@ var main = function () {
 		}
 	});
 
-	// добавляем вкладку Теги
+	// добавляем вкладку Врачи
 	tabs.push({
-		"name": "Теги",
+		"name": "Врачи",
 		"content":function (callback) {
-			$.get("todos.json", function (toDoObjects) {	
-				// создание $content для Теги 
-				var organizedByTag = organizeByTags(toDoObjects),
-					$content;
-				organizedByTag.forEach(function (tag) {
-					var $tagName = $("<h3>").text(tag.name);
-						$content = $("<ul>");
-					tag.toDos.forEach(function (description) {
-						var $li = $("<li>").text(description);
-						$content.append($li);
-					});
-					$("main .content").append($tagName);
-					$("main .content").append($content);
-				});
-				callback(null,$content);
-			}).fail(function (jqXHR, textStatus, error) {
-				// в этом случае мы отправляем ошибку вместе с null для $content
-				callback(error, null);
-			});
-		}
-	});
-
-	// создаем вкладку Добавить
-	tabs.push({
-		"name": "Добавить",
-		"content":function () {
-			$.get("todos.json", function (toDoObjects) {	
-				// создание $content для Добавить 
-				var $input = $("<input>").addClass("description"), 
-					$textInput = $("<p>").text("Введите новую задачу: "),
-					$tagInput = $("<input>").addClass("tags"),
-					$tagLabel = $("<p>").text("Тэги: "),
-					$button = $("<button>").text("+");
-				$("main .content").append($textInput).append($input).append($tagLabel).append($tagInput).append($button); 
-				function btnfunc() {
-					var description = $input.val(),
-						tags = $tagInput.val().split(","),
-						// создаем новый элемент списка задач
-						newToDo = {"description":description, "tags":tags};
-					$.post("todos", newToDo, function(result) {
-						$input.val("");
-						$tagInput.val("");
-						$(".tabs a:first-child span").trigger("click");
-					});
-				}
-				$button.on("click", function() {
-					btnfunc();
-				});
-				$('.tags').on('keydown',function(e){
-					if (e.which === 13) {
-						btnfunc();
+			$.getJSON("/users.json", function (usersObjects) {
+				var $content,
+					i;
+				$content = $("<ul>");
+				for (i = 0; i < usersObjects.length; i++) {
+					if (usersObjects[i].doctor) {
+						var $usersListItem = liaWithDeleteOnClick(usersObjects[i], function() {
+							$(".tabs a:nth-child(2) span").trigger("click");
+						});
+						$content.append($usersListItem);	
 					}
-				});
+				}
+				callback(null, $content);
+			}).fail(function(jqXHR, textStatus, error) {
+				callback(error, null);
 			});
 		}
 	});
@@ -191,7 +148,6 @@ var main = function () {
 			$(".tabs a span").removeClass("active");
 			$spanElement.addClass("active");
 			$("main .content").empty();
-			$("main .content").append("<h2>").text("Список записей к врачам: ")
 			tab.content(function (err, $content) {
 				if (err !== null) {
 					alert ("Возникла проблема при обработке запроса: " + err);
@@ -206,6 +162,7 @@ var main = function () {
 	$(".tabs a:first-child span").trigger("click");
 }
 
+
 $(document).ready(function() {
-		main();
+	main();
 });
